8122dc6191c7003fc5bed88da3954ac744a9af0d
Hennmeister <henning.lindig@rogers.com>
2020-01-15 14:58:59 -0500

    Refactored to Sk-learn modules directl .Now doing more feature extraction and using soft voting for ensemble classifer.

--- train_model.py
+++ train_model.py
@@ -9,28 +9,34 @@ from sklearn.svm import LinearSVC
 from sklearn.ensemble import VotingClassifier, RandomForestClassifier
 from sklearn.linear_model import LogisticRegressionCV
 
+import pickle
+
 def train_classifier():
     vectorizer = CountVectorizer(analyzer="word", \
                                  max_df = 0.9, \
                                  min_df = 2, \
-                                 max_features=5000,
+                                 # max_features=5000,
                                  )
 
     data_x = []
     data_y = []
+    num_pos = num_neg = 0
     with open('Data/Tweets/cleaned_negTweets', encoding="ISO-8859-1", mode='r') as output_neg:
         line = output_neg.readline().rstrip()
         while line:
             data_x.append(line)
             data_y.append(0)
             line = output_neg.readline()
+            num_neg += 1
     with open('Data/Tweets/cleaned_posTweets', mode='r', encoding="ISO-8859-1") as output_pos:
         line = output_pos.readline()
         while line:
             data_x.append(line)
             data_y.append(1)
             line = output_pos.readline()
+            num_pos += 1
 
+    print('num_pos: ', num_pos, 'num_neg: ', num_neg)
     data_x_train, data_x_test, data_y_train, data_y_test = \
         train_test_split(data_x, data_y, train_size=(0.8), random_state=14)
 
@@ -43,22 +49,24 @@ def train_classifier():
     data_x_train = TfidfTransformer().fit_transform(data_x_train)
     print('datax_train shape', data_x_train.shape)
 
-    clf = VotingClassifier([('lsvc', CalibratedClassifierCV(LinearSVC(max_iter=10000))),
-                            ('mnb', MultinomialNB()),
-                            ('r', RandomForestClassifier(n_estimators=100)),
-                           ('lgrg', LogisticRegressionCV(random_state=1)),
-                           ('bnb', BernoulliNB())], voting='soft')
+    ensemble_classifier = VotingClassifier([('lsvc', CalibratedClassifierCV(LinearSVC())),
+                                            ('mnb', MultinomialNB()),
+                                            ('r', RandomForestClassifier(n_estimators=100)),
+                                            ('lgrg', LogisticRegressionCV(random_state=1, max_iter=50000)),
+                                            ('bnb', BernoulliNB())], voting='soft')
 
-    clf = clf.fit(data_x_train, data_y_train)
-    print('clf score', clf.score(data_x_test, data_y_test))
+    ensemble_classifier = ensemble_classifier.fit(data_x_train, data_y_train)
+    print('clf score', ensemble_classifier.score(data_x_test, data_y_test))
 
-    y_predicted = clf.predict(data_x_test)
+    y_predicted = ensemble_classifier.predict(data_x_test)
 
     from sklearn.metrics import confusion_matrix
     print(confusion_matrix(y_predicted, data_y_test))
 
     from sklearn.metrics import classification_report
     print(classification_report(data_y_test, y_predicted))
+
+    pickle.dump(ensemble_classifier, open("ensemble_classifer.pickle", 'wb'))
 
 if __name__ == '__main__':
     train_classifier()
